b1155c598cfc6d00cd51ce9ba95fbc6cf674fafa
Elijah Lieu <elijahlieu@protonmail.com>
2022-06-12 16:26:33 -0400

    removing compression

--- site/src/mapcolorizer.tsx
+++ site/src/mapcolorizer.tsx
@@ -1,17 +1,18 @@
-import { Title } from "@mantine/core";
+import { Text, Title } from "@mantine/core";
 // @ts-ignore
 import { Unity, useUnityContext } from "react-unity-webgl";
 const MapColorizer = () => {
-  const { unityProvider } = useUnityContext({
+  const { unityProvider, isLoaded } = useUnityContext({
     loaderUrl: "build.loader.js",
-    dataUrl: "build.data.gz",
-    frameworkUrl: "build.framework.js.gz",
-    codeUrl: "build.wasm.gz",
+    dataUrl: "build.data",
+    frameworkUrl: "build.framework.js",
+    codeUrl: "build.wasm",
   });
 
   return (
     <section>
       <Title order={2}>Map</Title>
+      {!isLoaded && <Text>Loading...</Text>}
       <Unity
         unityProvider={unityProvider}
         style={{ height: "600px", width: "960px" }}
